3cb310331972205d4308d2e9e80a42a98f248eba
Seth A. Agudey <137450706+sethagudey@users.noreply.github.com>
2026-05-29 14:00:46 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,73 @@
-// =====================================
-// INITIALIZE APP
-// =====================================
+// ======================================
+// INITIALIZE APPLICATION
+// ======================================
 
-displayProducts();
+window.addEventListener(
+  "DOMContentLoaded",
+  () => {
 
-updateCart();
+    // LOAD PRODUCTS
 
-// =====================================
-// ORDER BUTTON
-// =====================================
+    displayProducts();
 
-document
-  .getElementById("orderBtn")
-  .addEventListener(
+    // LOAD CART
+
+    updateCart();
+
+    // LOAD ORDERS
+
+    if (
+
+      typeof renderOrders ===
+      "function"
+
+    ) {
+
+      renderOrders();
+
+    }
+
+  }
+);
+
+// ======================================
+// ORDER BUTTON EVENT
+// ======================================
+
+const orderBtn =
+  document.getElementById(
+    "orderBtn"
+  );
+
+if (orderBtn) {
+
+  orderBtn.addEventListener(
     "click",
     submitOrder
   );
+
+}
+
+// ======================================
+// CONTINUE SHOPPING
+// ======================================
+
+const successHomeBtn =
+  document.getElementById(
+    "successHomeBtn"
+  );
+
+if (successHomeBtn) {
+
+  successHomeBtn.addEventListener(
+    "click",
+    () => {
+
+      clearCart();
+
+      showProductsPage();
+
+    }
+  );
+
+}
