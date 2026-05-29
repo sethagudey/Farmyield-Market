8e38440a643b93948fdc24d7fb75c8cb8b891255
Seth A. Agudey <137450706+sethagudey@users.noreply.github.com>
2026-05-29 16:27:09 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -119,3 +119,123 @@ if (successHomeBtn) {
   );
 
 }
+
+// ======================================
+// PRODUCT MODAL
+// ======================================
+
+const productModal =
+  document.getElementById(
+    "productModal"
+  );
+
+const modalImage =
+  document.getElementById(
+    "modalImage"
+  );
+
+const modalTitle =
+  document.getElementById(
+    "modalTitle"
+  );
+
+const modalPrice =
+  document.getElementById(
+    "modalPrice"
+  );
+
+const modalDescription =
+  document.getElementById(
+    "modalDescription"
+  );
+
+const modalStock =
+  document.getElementById(
+    "modalStock"
+  );
+
+const closeModal =
+  document.getElementById(
+    "closeModal"
+  );
+
+// ======================================
+// OPEN MODAL
+// ======================================
+
+function openProductModal(productId) {
+
+  // FIND PRODUCT
+
+  const product =
+    products.find(
+      item => item.id === productId
+    );
+
+  // UPDATE MODAL
+
+  modalImage.src =
+    product.image;
+
+  modalTitle.innerText =
+    product.name;
+
+  modalPrice.innerText =
+
+    `GHS ${product.pricePerKg} per Kg`;
+
+  modalDescription.innerText =
+
+    product.description;
+
+  modalStock.innerText =
+
+    product.stock;
+
+  // SHOW MODAL
+
+  productModal.classList.remove(
+    "hidden"
+  );
+
+}
+
+// ======================================
+// CLOSE MODAL
+// ======================================
+
+closeModal.addEventListener(
+  "click",
+  () => {
+
+    productModal.classList.add(
+      "hidden"
+    );
+
+  }
+);
+
+// ======================================
+// CLOSE ON OUTSIDE CLICK
+// ======================================
+
+window.addEventListener(
+  "click",
+  (event) => {
+
+    if (
+
+      event.target ===
+      productModal
+
+    ) {
+
+      productModal.classList.add(
+        "hidden"
+      );
+
+    }
+
+  }
+);
+
