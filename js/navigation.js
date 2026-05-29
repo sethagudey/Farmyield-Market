48ae94728d16c5c9b6e889a831766b0851a58d93
Seth A. Agudey <137450706+sethagudey@users.noreply.github.com>
2026-05-29 10:17:05 +0000

    Create navigation.js

---
+++ js/navigation.js
@@ -0,0 +1,123 @@
+// =====================================
+// PAGE ELEMENTS
+// =====================================
+
+const productsPage =
+  document.getElementById(
+    "productsPage"
+  );
+
+const checkoutPage =
+  document.getElementById(
+    "checkoutPage"
+  );
+
+const supportPage =
+  document.getElementById(
+    "supportPage"
+  );
+
+// =====================================
+// NAVIGATION FUNCTIONS
+// =====================================
+
+function showProductsPage() {
+
+  productsPage.classList.remove(
+    "hidden"
+  );
+
+  checkoutPage.classList.add(
+    "hidden"
+  );
+
+  supportPage.classList.add(
+    "hidden"
+  );
+
+}
+
+function showCheckoutPage() {
+
+  productsPage.classList.add(
+    "hidden"
+  );
+
+  checkoutPage.classList.remove(
+    "hidden"
+  );
+
+  supportPage.classList.add(
+    "hidden"
+  );
+
+}
+
+function showSupportPage() {
+
+  productsPage.classList.add(
+    "hidden"
+  );
+
+  checkoutPage.classList.add(
+    "hidden"
+  );
+
+  supportPage.classList.remove(
+    "hidden"
+  );
+
+}
+
+// =====================================
+// EVENTS
+// =====================================
+
+document
+  .getElementById("homeNav")
+  .addEventListener(
+    "click",
+    showProductsPage
+  );
+
+document
+  .getElementById("supportNav")
+  .addEventListener(
+    "click",
+    showSupportPage
+  );
+
+document
+  .getElementById("backBtn")
+  .addEventListener(
+    "click",
+    showProductsPage
+  );
+
+document
+  .getElementById("supportBackBtn")
+  .addEventListener(
+    "click",
+    showProductsPage
+  );
+
+document
+  .getElementById("goToCheckout")
+  .addEventListener(
+    "click",
+    () => {
+
+      if (cart.length === 0) {
+
+        alert(
+          "Please add products to cart."
+        );
+
+        return;
+
+      }
+
+      showCheckoutPage();
+
+    }
+  );
